cdf0d9872d70680b9a535b5ebb80d11ce9e48310
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2023-09-15 12:35:34 -0300

    addCarinho melhorando visual

--- src/menuCarinho.js
+++ src/menuCarinho.js
@@ -1,5 +1,14 @@
 import {catalogo } from "./utilidades"
-
+const idPrComQtd = {
+  // '1':0,
+  // '2':0,
+  // '3':0,
+  // '4':0,
+  // '5':0,
+  // '6':0,
+  // '7':0,
+  // '8':0,
+}
 function abrirCarrinho(){
     document.querySelector('#carrinho').classList.add('right-[0]')
     document.querySelector('#carrinho').classList.remove('right-[-360px]')
@@ -16,8 +25,27 @@ export function inicializarCarrinho(){
     btnFechar.addEventListener('click', fecharCarrinho);
     btnAbrir.addEventListener("click", abrirCarrinho)
 }// export como se eu deixasse acessesavel para os outros arquivos js
+function atuaInfQt(idProduto){
+  document.querySelector(`#qtd-${idProduto}`).innerText = idPrComQtd[idProduto]
+    }
 
+function increQtdPr(idProduto) {
+idPrComQtd[idProduto]++;
+atuaInfQt(idProduto)
+}
+function decreQtdPr(idProduto) {
+  idPrComQtd[idProduto]--;
+  atuaInfQt(idProduto)
+  }
+ 
 export function addCarinho(idProduto){
+  if(idProduto in idPrComQtd){//in existe no objeto
+  
+increQtdPr(idProduto)
+return// sair da função 
+  }
+  //agora não temos prdutos   repetidos
+  idPrComQtd[idProduto] = 1 // o proprio js esta criando para mim um propriedade 
   const produto = catalogo.find((p) => p.id === idProduto)// find e ache, vamo achar um produto inpaticular  => tal que esses produto tenha um id ingual produto idProduto, então quem chamr essa função tem que passar seu id e iram isar esses id pegar intens do catalogo 
   const containerProdutoCarrinho = document.querySelector('#Produtos-carinhos');
 
@@ -25,11 +53,16 @@ export function addCarinho(idProduto){
 `<article class="flex bg-slate-100 rounded-lg p-1 relative">
     <button id="fechar-produto" class=" absolute top-0 right-2"><i class="fa-solid fa-circle-xmark  text-xl text-slate-600 hover:text-slate-900"></i></button>
     <img src="./assets/img/${produto.nomeImg}" alt="Produto adicionado como nome de: ${produto.altProduto}" class="h-24 rounded-lg">
-    <div class="py-2">
+  <div class="p-3 px flex flex-col justify-between ">
       <p class="text-slate-900 text-sm">${produto.nome}</p>
     <p class="text-green-700 text-lg">$${produto.precoProduto}</p>
     </div>
-   </article>`
+    <div class='flex text-slate-950 items-end absolute bottom-0 right-2 gap-2 text-lg'>
+      <button>-</button>
+      <p id='qtd-${produto.id}'>${idPrComQtd[produto.id]}</p>
+      </button>+<button>
+    </div>
+   </article>`;
    
    containerProdutoCarrinho.innerHTML += cartaoProdutoCarinho
 }
--- src/utilidades.js
+++ src/utilidades.js
@@ -1,6 +1,6 @@
 export const catalogo = [
     {
-        id:1,
+        id:'1',
         nome:'One Piece',
         volume:180,
         encadernacao:'Encadernação Clássica',
@@ -9,7 +9,7 @@ export const catalogo = [
         altProduto:'produto 1 imagem manga de one piece'
     },
     {
-        id:2,
+        id:'2',
         nome:'Jujutsu Kaisen: Batalha De Feiticeiros',
         volume: 22,
         encadernacao:'',
@@ -18,7 +18,7 @@ export const catalogo = [
         altProduto:'produto 2 imagem manga de Jujutsu Kaisen'
     },
     {
-        id:3,
+        id:'3',
         nome:'Zom 100 - Coisas para fazer antes de virar zumbi',
         volume: 1,
         encadernacao:'',
@@ -27,7 +27,7 @@ export const catalogo = [
         altProduto:'produto 3 imagem manga de Zom 100'
     },
     {
-        id:4,
+        id:'4',
         nome:'Spy X Family',
         volume: 2,
         encadernacao:'',
@@ -36,7 +36,7 @@ export const catalogo = [
         altProduto:'produto 4 imagem manga de Spy X Family '
     },
     {
-        id:5,
+        id:'5',
         nome:'Cavaleiros do Zodiaco The Lost Canvas Gaiden Especial',
         volume: 8,
         encadernacao:'',
@@ -45,7 +45,7 @@ export const catalogo = [
         altProduto:'produto 5 imagem manga de Cavaleiros do Zodiaco The Lost Canvas Gaiden Especial '
     },
     {
-        id:6,
+        id:'6',
         nome:'Berserk',
         volume: 2,
         encadernacao:'',
@@ -55,7 +55,7 @@ export const catalogo = [
         altProduto:'produto 6 imagem manga de Berserk '
     },
     {
-        id:7,
+        id:'7',
         nome:'Blue Lock',
         volume: 2,
         encadernacao:'',
@@ -64,7 +64,7 @@ export const catalogo = [
         altProduto:'produto 7 imagem manga de Blue Lock '
     },
     {
-        id:8,
+        id:'8',
         nome:'Gokushufudou - Tatsu Imortal',
         volume: 1,
         encadernacao:'',
